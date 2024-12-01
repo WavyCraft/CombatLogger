fbe869183f9d3a9f304597aff54761740e3a86d6
WavyCraft <wavycraftnetwork@gmail.com>
2024-11-30 21:17:56 -0800

    Update CombatManager.php

--- src/wavycraft/combatlogger/CombatManager.php
+++ src/wavycraft/combatlogger/CombatManager.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace wavycraft\combatlog;
 
 use pocketmine\player\Player;
@@ -8,69 +10,84 @@ use pocketmine\Server;
 
 use pocketmine\utils\SingletonTrait;
 
-use function array_map;
-use function count;
-use function max;
-
 use core\kitpvp\utils\KDRManager;
-
 use core\utils\TextColor;
 
 final class CombatManager {
     use SingletonTrait;
 
-    private array $combatLog = [];
-
+    private array $combatGroups = [];
     private const COMBAT_DURATION = 15;
 
-    public function setCombatState(Player $victim, Player $damager) : void{
-        $uuid = $victim->getUniqueId()->getBytes();
-        $damagerName = $damager->getName();
+    public function setCombatState(Player $playerA, Player $playerB) : void{
+        $playerAUuid = $playerA->getUniqueId()->getBytes();
+        $playerBUuid = $playerB->getUniqueId()->getBytes();
 
-        if (!isset($this->combatLog[$uuid])) {
-            $this->combatLog[$uuid] = [];
+        $groupId = $this->getCombatGroupId($playerAUuid, $playerBUuid);
+        if ($groupId === null) {
+            $groupId = uniqid("combat_", true);
+            $this->combatGroups[$groupId] = [];
         }
 
-        $this->combatLog[$uuid][$damagerName] = time() + self::COMBAT_DURATION;
-
-        Loader::getInstance()->getScheduler()->scheduleRepeatingTask(new CombatTask($victim), 20);
-        $victim->sendMessage(TextColor::RED . "You are in combat! You cannot log out for 15 seconds...");
+        $this->combatGroups[$groupId][$playerAUuid] = time() + self::COMBAT_DURATION;
+        $this->combatGroups[$groupId][$playerBUuid] = time() + self::COMBAT_DURATION;
+        foreach ([$playerA, $playerB] as $player) {
+            $player->sendMessage(TextColor::RED . "You are in combat! You cannot log out for 15 seconds...");
+        }
     }
 
-    public function resetCombatTimer(Player $player) : void{
-        $uuid = $player->getUniqueId()->getBytes();
-        if (isset($this->combatLog[$uuid])) {
-            foreach ($this->combatLog[$uuid] as $opponentName => &$combatEndTime) {
+    private function getCombatGroupId(string $uuidA, string $uuidB): ?string {
+        foreach ($this->combatGroups as $groupId => $group) {
+            if (isset($group[$uuidA]) || isset($group[$uuidB])) {
+                return $groupId;
+            }
+        }
+        return null;
+    }
+
+    public function resetCombatTimer(Player $player): void {
+        $playerUuid = $player->getUniqueId()->getBytes();
+        $groupId = $this->getCombatGroupId($playerUuid, $playerUuid);
+
+        if ($groupId !== null) {
+            foreach ($this->combatGroups[$groupId] as $uuid => &$combatEndTime) {
                 $combatEndTime = time() + self::COMBAT_DURATION;
             }
         }
     }
 
-    public function handlePlayerDisconnect(Player $player) : void{
-        $uuid = $player->getUniqueId()->getBytes();
-        if (isset($this->combatLog[$uuid])) {
-            foreach ($this->combatLog[$uuid] as $opponentName => $combatEndTime) {
-                if (time() <= $combatEndTime) {
-                    $opponent = Server::getInstance()->getPlayerExact($opponentName);
-                    if ($opponent !== null) {
-                        $opponent->sendMessage(TextColor::GREEN . "You have been credited for killing " . TextColor::YELLOW . $player->getName() . TextColor::GREEN . "!");
-                        KDRManager::getInstance()->addKill($opponent);
-                    }
+    public function handlePlayerDisconnect(Player $player): void {
+        $playerUuid = $player->getUniqueId()->getBytes();
+        $groupId = $this->getCombatGroupId($playerUuid, $playerUuid);
+
+        if ($groupId !== null) {
+            unset($this->combatGroups[$groupId][$playerUuid]);
+            foreach ($this->combatGroups[$groupId] as $remainingUuid => $combatEndTime) {
+                $remainingPlayer = Server::getInstance()->getPlayerByRawUUID($remainingUuid);
+                if ($remainingPlayer !== null && time() <= $combatEndTime) {
+                    $remainingPlayer->sendMessage(TextColor::GREEN . "Your opponent has left the combat zone!");
                 }
             }
-            unset($this->combatLog[$uuid]);
+
+            if (empty($this->combatGroups[$groupId])) {
+                unset($this->combatGroups[$groupId]);
+            }
         }
     }
 
     public function isInCombat(Player $player) : bool{
-        $uuid = $player->getUniqueId()->getBytes();
-        return isset($this->combatLog[$uuid]) && count($this->combatLog[$uuid]) > 0;
+        $playerUuid = $player->getUniqueId()->getBytes();
+        $groupId = $this->getCombatGroupId($playerUuid, $playerUuid);
+
+        return $groupId !== null && count($this->combatGroups[$groupId]) > 0;
     }
 
     public function getRemainingCombatTime(Player $player) : int{
-        $uuid = $player->getUniqueId()->getBytes();
-        if (isset($this->combatLog[$uuid])) {
-            $remainingTimes = array_map(fn($time) => $time - time(), $this->combatLog[$uuid]);
+        $playerUuid = $player->getUniqueId()->getBytes();
+        $groupId = $this->getCombatGroupId($playerUuid, $playerUuid);
+
+        if ($groupId !== null) {
+            $remainingTimes = array_map(fn($time) => $time - time(), $this->combatGroups[$groupId]);
             $remainingTime = max($remainingTimes);
             return $remainingTime > 0 ? $remainingTime : 0;
         }
